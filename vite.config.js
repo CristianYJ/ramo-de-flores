d6a36af512faee305e1c027ac481f46a498d8f95
CristianYJ <cristian10yj@gmail.com>
2025-11-11 21:33:29 -0600

    2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Asegúrate de que el nombre coincida con el repositorio
 export default defineConfig({
   plugins: [react()],
+  base: '/ramo-de-flores/',
 })
